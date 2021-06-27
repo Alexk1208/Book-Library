214ab97378f291ce4a707e49845c0b60da3afd0f
Alexander <alexkobayashi128@gmail.com>
2021-06-27 12:43:22 -0700

    changed the display to show inside a table instead of numerous separate divs. Merged add book and show library buttons in order to append everytime a new book is added

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let myLib = [];
-let library = document.querySelector('.library');
+let library = document.getElementById('library');
 
 function Book(title, author, pages, read){
     this.title = title
@@ -13,62 +13,85 @@ function addBookToLib(){
     let newAuthor = document.getElementById('authorInput').value;
     let pageNumber = document.getElementById('pageInput').value;
     let newRead = document.getElementById('readSelect').value;
+    if(newTitle === '' || newAuthor === '' || pageNumber === '' || newRead === 'default'){
+        return alert('Please Fill in All Details!')
+    }
     let newBook = new Book(newTitle, newAuthor, pageNumber, newRead);
     myLib.push(newBook);
 }
+function removeTextFromInput(){
+    document.getElementById('titleInput').value = '';
+    document.getElementById('authorInput').value = '';
+    document.getElementById('pageInput').value = '';
+    document.getElementById('readSelect').value = 'default';
+}
+
 
 function removeChildNodes(parent){
-    let childs = parent.childNodes;
-    while(childs.length > 0){
-        childs[0].remove();
+    let tableRow = parent.getElementsByTagName('tr')
+    while(tableRow.length > 1){
+        tableRow[1].parentNode.removeChild(tableRow[1]);
     }
 }
 
 function addBookName(object, appendLoc, i) {
-    let parentDiv = document.createElement('div')
-    let newBookDisplay = document.createElement('div');
+    let parentDiv = document.createElement('tr')
+    let titleDisplay = document.createElement('td');
+    let authorDisplay = document.createElement('td');
+    let pageDisplay = document.createElement('td');
     
     parentDiv.setAttribute('id', i);
     
-    newBookDisplay.innerHTML = 
-    `Title: ${object.title} Author: ${object.author} Pages: ${object.pages}`;
+    titleDisplay.innerHTML = object.title;
+    authorDisplay.innerHTML = object.author;
+    pageDisplay.innerHTML = object.pages;
 
     appendLoc.appendChild(parentDiv);
-    parentDiv.appendChild(newBookDisplay);
+    parentDiv.appendChild(titleDisplay);
+    parentDiv.appendChild(authorDisplay);
+    parentDiv.appendChild(pageDisplay);
     readButton(object, parentDiv);
     deleteButton(i, parentDiv);
 }
 
 function readButton(object, appendLoc){
+    let readButtonDisplay = document.createElement('td');
     let newReadButton = document.createElement('button');
-    newReadButton.innerHTML = 'Not Yet Read';
+    if(object.read === 'read'){
+        newReadButton.innerHTML = 'Read'
+    }
+    else{
+        newReadButton.innerHTML = 'Not Read'
+    }
     newReadButton.addEventListener('click', () =>{
-        if(object.read === 'no'){
-            newReadButton.innerHTML = 'Read';
-            object.read = 'yes'
+        if(object.read === 'read'){
+            newReadButton.innerHTML = 'Not Read';
+            object.read = 'notread'
         }
         else{
-            newReadButton.innerHTML = 'Not Yet Read';
-            object.read = 'no'
+            newReadButton.innerHTML = 'Read';
+            object.read = 'read'
         }
     })
-    appendLoc.appendChild(newReadButton);
+    readButtonDisplay.appendChild(newReadButton)
+    appendLoc.appendChild(readButtonDisplay);
 }
 
 function deleteButton(i, appendLoc){
+    let deleteButtonDisplay = document.createElement('td');
     let deleteBook = document.createElement('button')
     deleteBook.innerHTML = 'Delete';
     deleteBook.addEventListener('click', () =>{
         let deleteDOM = document.getElementById(i);
-        console.log(deleteDOM);
         deleteDOM.remove();
         delete myLib[i];
-        console.table(myLib)
     });
-    appendLoc.appendChild(deleteBook);
+    deleteButtonDisplay.appendChild(deleteBook);
+    appendLoc.appendChild(deleteButtonDisplay);
 }
 
 function displayAllBooks(){
+    removeTextFromInput();
     removeChildNodes(library);
     myLib = myLib.filter( Boolean);
     for(let i = 0; i < myLib.length; i++){
@@ -76,14 +99,11 @@ function displayAllBooks(){
     }
 }
 
+function appendBook(){
+    addBookToLib();
+    displayAllBooks();
+}
 
 
-document.querySelector('#newBookButton').addEventListener('click', addBookToLib);
-document.querySelector('#showEntireLib').addEventListener('click', displayAllBooks);
 
-// if(document.querySelector('#readSelect').value === 'notread'){
-//     newReadButton.innerHTML = 'Not Read';
-// }
-// else{
-//     newReadButton.innerHTML = 'Read';
-// }
+document.querySelector('#newBookButton').addEventListener('click', appendBook);
